478feb775a68043a957acef6d060226bf23fc375
Vladimir <dik2043@mail.ru>
2019-01-31 21:25:19 +0400

    created wholeObj

--- js/map.js
+++ js/map.js
@@ -1,9 +1,41 @@
+
 /* Получить случайное число */
 var mapPin = document.querySelector('.map__pins');
 
 var getRandomNumber = function (min, max) {
     return Math.round(Math.random() * (max - min) + min);
 };
+
+var generatedObjs = [];
+// var author = {'name':1};
+// var generatedObjs = [
+//     {
+//         'author': {
+//             'avatar': ,
+//         },
+//
+//         'offer': {
+//             'title': ,
+//             'adress': , 
+//             'price': ,
+//             'type': ,
+//             'rooms': ,
+//             'guests': ,
+//             'checkin': ,
+//             'checkout': ,
+//             'features': ,
+//             'description': ,
+//             'photos': 
+//         },
+//
+//         'location': {
+//             'x': ,
+//             'y': 
+//         }
+//     }
+// ];
+
+var counterSteps = 8;
 
 //      author      author      author      author      author      author
 
@@ -34,22 +66,22 @@ var title = [                                                       /* норм 
 ];
 console.log('var title - ' + title);
 
-var locationX = '300';  /* тестовое значение */
-var locationY = '350';  /* тестовое значение */
+// var locationX = '300';  /* тестовое значение */
+// var locationY = '350';  /* тестовое значение */
 
-var adress = locationX + ', ' + locationY;                           /* норм */
-console.log('var adress - ' + adress);
+// var adress = locationX + ', ' + locationY;                           /* норм */
+// console.log('var adress - ' + adress);
 
-var price = getRandomNumber(1000, 1000000);                          /* норм */
-console.log('var price - ' + price);
+// var price = getRandomNumber(1000, 1000000);                          /* норм */
+// console.log('var price - ' + price);
 
 var typeArr = ['palace', 'flat', 'house', 'bungalo'];                /* норм */
 var type = typeArr[getRandomNumber(0, typeArr.length - 1)];
 console.log('var type - ' + type);
 
 
-var rooms = getRandomNumber(1, 5);                                   /* норм */
-console.log('var rooms - ' + rooms);
+// var rooms = getRandomNumber(1, 5);                                   /* норм */
+// console.log('var rooms - ' + rooms);
 
 var guests = getRandomNumber(1, 25);                                 /* норм */
 console.log('var guests - ' + guests);
@@ -58,37 +90,119 @@ var checkinAndCheckout = ['12:00', '13:00', '14:00'];                /* норм
 var checkin = checkinAndCheckout[getRandomNumber(0, checkinAndCheckout.length - 1)];
 console.log('var checkin - ' + checkin);
 
-var checout = checkinAndCheckout[getRandomNumber(0, checkinAndCheckout.length - 1)];
-console.log('var checout - ' + checout);                             /* норм */
+var checkout = checkinAndCheckout[getRandomNumber(0, checkinAndCheckout.length - 1)];
+console.log('var checout - ' + checkout);                             /* норм */
 
 /* Заполняем массив features случайными элементами из featuresArr *без повторения* */
 
 var featuresArr = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var features = [];
+
 var toRecycleArr = function (newArr, oldArr) {
-    for (var i = 0; i <= getRandomNumber(0, oldArr.length - 1); i++) {
-        var transitArr = oldArr.concat();   /* копировать массив, чтобы не изменять стырый */
+    var transitArr = oldArr.concat();           /* копировать массив, чтобы не изменять стырый */
+    for (var i = 0; i <= getRandomNumber(0, oldArr.length - 1); i++) {          
         var random = getRandomNumber(0, transitArr.length - 1);                                            /* Как можно лучше? */
         newArr[i] = transitArr[random];
         transitArr.splice(random, 1);
     }
 };
-toRecycleArr(features, featuresArr);                                 /* норм */        
-console.log('var features - ' + features);
+// toRecycleArr(features, featuresArr);                           
+// console.log('var features - ' + features);
 
-var description = '';
-console.log('var description - ' + description);
+// var description = '';
+// console.log('var description - ' + description);
 
-var photos = [
+/* Заполняем массив photos случайными элементами из photosArr *без повторения* */
+
+var photosArr = [
     'http://o0.github.io/assets/images/tokyo/hotel1.jpg', 
     'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
     'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
-console.log('var photos - ' + photos);
+
+var toSortArr = function (newArr, arr) {
+    var transitArr = arr.concat();      /* копировать массив, чтобы не изменять стырый */
+    for (var i = 0; i <= arr.length - 1; i++) {
+        var random = getRandomNumber(0, transitArr.length - 1);  
+        newArr[i] = transitArr[random];
+        transitArr.splice(random, 1);
+    }
+};
+
+// console.log('var photos - ' + photos);
 
 //      location      location      location      location      location      location
 
 /* Координаты метки на карте */
 
-var x = getRandomNumber(1, mapPin.clientWidth);
-var y = getRandomNumber(130, 630);
+// var x = getRandomNumber(1, mapPin.clientWidth);
+// var y = getRandomNumber(130, 630);
+
+/* Создаем wrapper */
+
+var createWrapper = function () {
+    for (var i = 0; i < counterSteps; i++) {
+        var wrapper = {};
+        generatedObjs[i] = wrapper;
+    }
+};
+
+/* Создаем author */
+
+var createAuthorInObj = function (arr) {
+    for (var i = 0; i < counterSteps; i++) {
+        var wrapper = generatedObjs[i];
+        var author = {};
+        wrapper[0] = author;
+        author.avatar = arr[i];
+    }
+};
+
+/* Создаем location */
+
+var createLocationInObjs = function () {
+    for (var i = 0; i < counterSteps; i++) {
+        var wrapper = generatedObjs[i];
+        var locationXY = {};
+        wrapper[2] = locationXY;
+        locationXY.x = getRandomNumber(0, mapPin.clientWidth);
+        locationXY.y = getRandomNumber(130, 630);
+    }
+};
+
+/* Создаем offer */
+
+var createOfferInObj = function (title) {
+    for (var i = 0; i < counterSteps; i++) {
+        var photos = [];
+        var features = [];
+        toRecycleArr(features, featuresArr);
+        toSortArr(photos, photosArr);
+        var wrapper = generatedObjs[i];
+        var offer = {};
+        wrapper[1] = offer;
+        offer.title = title[i];
+        offer.adress = generatedObjs[i][2].x + ', ' + generatedObjs[i][2].y;
+        offer.price = getRandomNumber(1000, 1000000);
+        offer.type = typeArr[getRandomNumber(0, typeArr.length - 1)];
+        offer.rooms = getRandomNumber(1, 5);
+        offer.guests = getRandomNumber(1, 25);
+        offer.checkin = checkinAndCheckout[getRandomNumber(0, checkinAndCheckout.length - 1)];
+        offer.checkout = checkinAndCheckout[getRandomNumber(0, checkinAndCheckout.length - 1)];
+        offer.features = features;
+        offer.descrription = '';
+        offer.photos = photos;
+    }  
+};
+
+/* Все целиком */
+
+var createWholeObj = function() {
+    createWrapper();
+    createAuthorInObj(avatars);
+    createLocationInObjs();
+    createOfferInObj(title);
+}
+
+createWholeObj();
+
+console.log(generatedObjs);

655dfa4897df6c2e53e6d9dd1cf7d630d55800c5
Vladimir <dik2043@mail.ru>
2019-02-12 15:05:37 +0400

    закончил задание 16. Вроде

--- js/map.js
+++ js/map.js
@@ -314,7 +314,110 @@ mapPinMain.addEventListener('mouseup', function (evt) {
     }
 });
 
+// Задание 16   Задание 16   Задание 16   Задание 16   Задание 16   Задание 16   
 
+
+/* Проверка типа жилья */
+
+var selectType = document.querySelector('#type');
+var inputPrice = document.querySelector('#price');
+var inputTitle = document.querySelector('#title');
+
+var showWrongSet = function (evt) {
+    evt.target.style.border = '3px solid red';    
+};
+
+inputTitle.addEventListener('invalid', function (evt) {
+    showWrongSet(evt);
+});
+
+inputPrice.addEventListener('invalid', function (evt) {
+    showWrongSet(evt);
+});
+
+selectType.addEventListener('change', function (evt) {
+    checkFlat(evt);
+});
+
+var checkFlat = function (evt) {
+    switch (selectType.value) {
+        case 'flat':
+            inputPrice.setAttribute('min', '1000');
+            inputPrice.setAttribute('placeholder', '1000');
+            break;
+
+        case 'bungalo':
+            inputPrice.setAttribute('min', '0');
+            inputPrice.setAttribute('placeholder', '0');
+            break;
+
+        case 'house':
+            inputPrice.setAttribute('min', '5000');
+            inputPrice.setAttribute('placeholder', '5000');
+            break;
+
+        case 'palace':
+            inputPrice.setAttribute('min', '10000');
+            inputPrice.setAttribute('placeholder', '10000');
+            break;
+    }
+};
+
+/* Проверка времени заезда */
+
+var timeIn = document.querySelector('#timein');
+var timeOut = document.querySelector('#timeout');
+
+timeIn.addEventListener('change', function (evt) {
+    checkTimeIn(evt);
+});
+
+timeOut.addEventListener('change', function (evt) {
+    checkTimeOut(evt);
+});
+
+var checkTimeIn = function (evt) {
+    timeOut.value = timeIn.value;
+};
+
+var checkTimeOut = function (evt) {
+    timeIn.value = timeOut.value;
+};
+
+/* Проверка гостей */
+
+var roomsQuantity = document.querySelector("#room_number");
+var guestsQuantity = document.querySelector("#capacity");
+guestsQuantity.value = '1';
+
+roomsQuantity.addEventListener('change', function (evt) {
+    checkGuestsAndRooms(evt);
+});
+
+guestsQuantity.addEventListener('change', function (evt) {
+    checkGuestsAndRooms(evt);
+});
+
+guestsQuantity.addEventListener('invalid', function (evt) {
+    showWrongSet(evt);
+});
+
+var checkGuestsAndRooms = function (evt) {
+    if (roomsQuantity.value === '1' && !(guestsQuantity.value === '1')) {       /* 1 комната и не один гость */
+        guestsQuantity.setCustomValidity('При одной комнате нужно указать одного гостя');
+        console.log('atatta');
+    } else if ((roomsQuantity.value === '2' && !(guestsQuantity.value === '1'))         /* 2 комнаты и не один и не два гостя */
+        || (roomsQuantity.value === '2' && !(guestsQuantity.value === '2'))) {
+        guestsQuantity.setCustomValidity('При двух комнатах нужно указать одного или двух гостей');
+        console.log('atatta 23');
+    } else if (roomsQuantity.value === '3' && (guestsQuantity.value === '0')) {     /* 3 комнаты и ни одного гостя */
+        guestsQuantity.setCustomValidity('При трех комнатах нельзя использовать вариант \'не для гостей\'');
+    } else if (roomsQuantity.value === '100' && !(guestsQuantity.value === '0')) {      /* 100 комнат и !не! нет гостей */
+        guestsQuantity.setCustomValidity('Вам нельзя гостей :(');
+    } else {
+        guestsQuantity.setCustomValidity('');
+    }
+};
 
 
 // весь алгоритм программы (условно)
@@ -326,10 +429,12 @@ mapPinMain.addEventListener('mouseup', function (evt) {
 /* 5 переводим страницу прикрепляем первом запуске в нективное состояние */
 /* 6 создаем обработчики на разные сценарии взаимодействия */
 /* 7 навешиваем обработчики на главную метку */
+/* 8 Проверяем все поля в форме объявления */
 
 
 // заметки
 /* епте как-то сложновато,
  в консоли браузера свойства объекта идут не в том порядке (location перед offer),
  если взять больше восьми объектов, будет беда
- title идет не в случайном порядке */
+ title идет не в случайном порядке 
+ А как потом убрать красную рамку с неправильных полей?*/
